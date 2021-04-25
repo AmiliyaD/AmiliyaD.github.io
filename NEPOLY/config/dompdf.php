a27da2351296f013009acb0000c8db05ea08afbc
Genderless <49407094+GenderlessChild@users.noreply.github.com>
2021-04-25 23:21:57 +0600

    Русский текст в PDF

--- NEPOLY/config/dompdf.php
+++ NEPOLY/config/dompdf.php
@@ -149,7 +149,7 @@ return array(
          * Used if no suitable fonts can be found. This must exist in the font folder.
          * @var string
          */
-        "default_font" => "serif",
+        "default_font" => "dejavu sans condensed",
 
         /**
          * Image DPI setting
--- NEPOLY/resources/views/pdfView.blade.php
+++ NEPOLY/resources/views/pdfView.blade.php
@@ -1,24 +1,87 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>Document</title>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>{{$historyPar[0]->title}}</title>
 </head>
+
 <body>
     <div class="container">
-        <div class="row">
-            <h1>{{$historyPar[0]->title}}</h1>
-            <p>{{$historyPar[0]->text}}</p>
-        </div>
-        <div class="row">
-            @foreach ($historyTexts as $item)
-                {{$item->history_title}}
+        {{-- title --}}
+        <div class="row title">
+            <div class="col-md-12">
+                <h1 class="title_h1 text-center">{{$historyPar[0]->title}}</h1>
+                <span>Автор: {{$historyPar[0]->userId->name}}</span>
+                <br>
+                <span>Статус: {{$historyPar[0]->status}}</span>
+            </div>
+            <div class="col-md-12">
+                <p class="desc">{{$historyPar[0]->text}}</p>
+            </div>
 
-                {{$item->history_text}}
-            @endforeach
+        </div>
+
+        
+        {{-- text --}}
+        <div class="row text">
+            <div class="col-md-12">
+                
+                @if (count($historyTexts) == 0)
+                На данный момент работа не содержит глав.
+                @endif
+                @foreach ($historyTexts as $item)
+                <div class="text_body">
+                    <b>
+                        <p class="text_title">{{$item->history_title}}</p>
+                    </b>
+    
+                    <p class="text_content">{{$item->history_text}}</p>
+
+                </div>
+              
+                @endforeach
+            </div>
+
         </div>
     </div>
 </body>
+<style>
+    @font-face {
+        font-family: "DejaVu Sans";
+        src: url("/fonts/dejavu-sans/DejaVuSans.ttf");
+        /* IE9 Compat Modes */
+        src:
+            local("DejaVu Sans"),
+            local("DejaVu Sans"),
+            url("/fonts/dejavu-sans/DejaVuSans.ttf");
+    }
+
+    h1,
+    .text_title,
+    .desc {
+        text-align: center;
+    }
+
+    body {
+        font-family: "DejaVu Sans";
+        font-size: 12px;
+    }
+
+    .text {
+        margin-bottom: 150px;
+        margin-top: 100px;
+    }
+.text_body {
+   margin-top: 100px;
+}
+    .text_title {
+        font-size: 20px;
+    }
+
+</style>
+
 </html>
